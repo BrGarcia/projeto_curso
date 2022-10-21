9692b359dc122993d708122b28721973f204a06f
Bruno <brgarcia@outlook.com>
2022-10-21 17:09:08 -0300

    initial commit

---
+++ aula.py
@@ -0,0 +1,20 @@
+perguntas = { 'pergunta1':{
+                'questao':'quanto é 2+2?',
+                    'resposta' : {'A':'1', 'B': '2', 'C':'4', 'D': '6'},
+                    'gabarito': 'C'},
+                'pergunta2':{
+                'questao':'quanto é 2+1?',
+                    'resposta' : {'A':'3', 'B': '2', 'C':'4', 'D': '6'},
+                    'gabarito': 'A'},
+                'pergunta3':{
+                'questao':'quanto é 2+4?',
+                    'resposta' : {'A':'1', 'B': '2', 'C':'4', 'D': '6'},
+                    'gabarito': 'D'},         
+}
+
+}
+
+
+
+
+}
