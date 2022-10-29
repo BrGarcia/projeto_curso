e641ad8a7c663ba5f3c8ddf43fb66668b43fd86d
Bruno <brgarcia@outlook.com>
2022-10-29 11:32:58 -0300

    programa_finalizado

--- busca_repetido.py
+++ busca_repetido.py
@@ -1,27 +1,10 @@
-import random as rd
-
-def gerador_lista(qtd:int, tamanho:int)->list:
-    """Gera uma lista com diversas listas randomicas"""
-    lista = []
-    listatemp = []
-    for j in range(0, qtd):
-        for r in range(0, tamanho):
-            listatemp.append(rd.randint(0, 9))
-        lista.append(listatemp)
-        listatemp = []
-    return lista
-lista_teste = [[4, 7, 0, 0, 4, 9],[1, 6, 0, 3, 1, 0],[2, 0, 1, 3, 7, 5]]
-listinha_teste = [2, 0, 1, 3, 7, 5, 7]
-#listalista = gerador_lista(1,6)
-#for i, numero in enumerate(lista_teste):
-#    print(i, numero)
-for i in range(3):
-    lista_temp = listinha_teste
-    print(lista_temp)
-    var_temporaria = lista_temp[i]
-    lista_temp.pop(i)
-    print(lista_temp)
-    print(var_temporaria)
-    print(f"Listinha: {listinha_teste}")
-    lista_temp = listinha_teste
-    
+lista = [2, 2, 5, 7, 4, 5, 5, 0]
+lista_repetidos = []
+d_temp = -1
+for index,valor in enumerate(lista):
+    for i,v in enumerate(lista):
+        d = len(lista)-i
+        if (i != index) and (valor == v) and (i>index) and d>d_temp:
+            d_temp = d
+            lista_repetidos.clear()
+            lista_repetidos.append([i,v])
--- exercicio1.py
+++ exercicio1.py
@@ -1,39 +1,48 @@
+"""
+-> É uma lista de listas de números inteiros
+-> As listas internas tem o tamanho de 10 elementos
+-> As listas internas contém números entre 1 a 10 e eles podem ser duplicados
+Exercício
+-> Crie uma função que encontra o primeiro duplicado considerando o segundo
+    número como a duplicação. Retorne a duplicação considerada.
+        Requisitos:
+            A ordem do número duplicado é considerada a partir da segunda
+            ocorrência do número, ou seja, o número duplicado em si.
+            Exemplo:
+                [1, 2, 3, ->3<-, 2, 1] -> 1, 2 e 3 são duplicados (retorne 3)
+                [1, 2, 3, 4, 5, 6] -> Retorne -1 (não tem duplicados)
+            Se não encontrar duplicados na lista, retorne -1 """
 import random as rd
 
 def gerador_lista(qtd:int, tamanho:int)->list:
-    """Gera uma lista com diversas listas randomicas"""
-    lista = []
+    """Gera uma lista de numeros randomicas com uma quantidade QTD com TAMANHO elementos"""
+    lista_local = []
     listatemp = []
-    for j in range(0, qtd):
-        for r in range(0, tamanho):
+    for _ in range(0, qtd):
+        for _ in range(0, tamanho):
             listatemp.append(rd.randint(0, 9))
-        lista.append(listatemp)
+        lista_local.append(listatemp)
         listatemp = []
-    return lista
+    return lista_local
 
-def busca_repetido(lista:list)->list:
+def busca_repetido(lista: list)->list:
     """"Recebe uma lista, busca o primeiro elemento repetido
     e retorna uma lista (a,b) onde a é o elemento repetido e 
     b é a posicao na lista do elemento repetido."""
-    for i, numero in enumerate(lista):
-        print(i, numero)
-        return True
-    
-listalista = gerador_lista(5,6)
-"""for i, numero in enumerate(listalista):
-    print(i, numero)"""
-     
-flag = bool(False)
-for num, lista in enumerate(listalista):
-    vlista = []
-    listav = []
-    for i, valor in enumerate(lista):
-        if flag:
-            flag = False
-        else:
-            a = valor
-            for v,j in enumerate(lista):
-                if (a == v) and (i > lista.index(v)):
-                        print(f"Lista n{num} {lista}: Item Repetido: {a} - Posicao na lista: {i}")
-                        flag = True
-                        break
+    lista_repetidos = [-1]
+    d_temp = -1
+    for index,valor in enumerate(lista):
+        for i,v in enumerate(lista):
+            d = len(lista)-i
+            if (i != index) and (valor == v) and (i>index) and d>d_temp:
+                d_temp = d
+                lista_repetidos.clear()
+                lista_repetidos.append([i,v])
+    return lista_repetidos
+
+
+lista_main = gerador_lista(500,8)
+
+for i in lista_main:
+    lista_busca = busca_repetido(i)
+    print(i, lista_busca)
